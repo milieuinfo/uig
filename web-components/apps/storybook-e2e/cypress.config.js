30fb21c04cb55b6d66603c3c75625e3514c01267
Kris Speltincx <kris@bladerbeeld.be>
2022-09-28 16:20:41 +0200

    code aan het overnemen uit de vorige repo: uig-webcomponents/nx  - de stories toegevoegd

--- web-components/apps/storybook-e2e/cypress.config.js
+++ web-components/apps/storybook-e2e/cypress.config.js
@@ -8,6 +8,10 @@ module.exports = defineConfig({
     videosFolder: '../../dist/cypress/apps/storybook-e2e/videos',
     screenshotsFolder: '../../dist/cypress/apps/storybook-e2e/screenshots',
     chromeWebSecurity: false,
+    reporter: '../../node_modules/cypress-multi-reporters',
+    reporterOptions: {
+        configFile: 'reporter-config.json'
+    },
     e2e: {
         setupNodeEvents(on, config) {
         },
