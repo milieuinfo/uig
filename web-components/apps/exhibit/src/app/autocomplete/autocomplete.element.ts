5be152a32c7ceee04adc36a437e75b797e770d8c
Kris Speltincx <kris@bladerbeeld.be>
2022-10-14 16:42:39 +0200

    autocomplete begint te werken in exhibit

--- web-components/apps/exhibit/src/app/autocomplete/autocomplete.element.ts
+++ web-components/apps/exhibit/src/app/autocomplete/autocomplete.element.ts
@@ -1,37 +1,26 @@
-import { css, html, LitElement, unsafeCSS } from 'lit';
-// import styles from './style/vl-autocomplete.scss';
-import styles from '../../../../../libs/elements/src/lib/_vl-elements.scss';
+const complexItems = [
+    { title: 'Gent', subtitle: 'Gemeente', value: '1' },
+    { title: 'Gentbos, Merelbeke', subtitle: 'Adres', value: '2' },
+    { title: 'Gentbruggestraat, Gent', subtitle: 'Adres', value: '3' },
+    { title: 'Gentele, Brugge', subtitle: 'Adres', value: '5' },
+    { title: 'Automotive Contractors Gent ', subtitle: 'Project', value: '6' },
+    { title: 'Buurtshuis Watersportbaan Gent', subtitle: 'Project', value: '7' },
+];
 
-export class AutocompleteElement extends LitElement {
-    static get styles() {
-        return [
-            css`
-                ${unsafeCSS(styles)}
-            `,
-        ];
+export class AutocompleteElement extends HTMLElement {
+    public static observedAttributes = [];
+
+    constructor() {
+        super();
+        const autocomplete = document.createElement('vl-autocomplete');
+        autocomplete.setAttribute('data-vl-min-chars', '1');
+        autocomplete.setAttribute('placeholder', 'Hint: typ Gent');
+        autocomplete['items'] = complexItems;
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+        shadowRoot.appendChild(autocomplete);
     }
 
-    // public static observedAttributes = [];
-
     // connectedCallback() {
-    //     const title = 'Autocomplete';
-    //     // const complexItems = [
-    //     //     { title: 'Gent', subtitle: 'Gemeente', value: '1' },
-    //     //     { title: 'Gentbos, Merelbeke', subtitle: 'Adres', value: '2' },
-    //     //     { title: 'Gentbruggestraat, Gent', subtitle: 'Adres', value: '3' },
-    //     //     { title: 'Gentele, Brugge', subtitle: 'Adres', value: '5' },
-    //     //     { title: 'Automotive Contractors Gent ', subtitle: 'Project', value: '6' },
-    //     //     { title: 'Buurtshuis Watersportbaan Gent', subtitle: 'Project', value: '7' },
-    //     // ];
-    //     const complexItems = [{ title: 'Gent', subtitle: 'Gemeente', value: '1' }];
-    //     const complexItemsParsed = JSON.stringify(complexItems);
-    //     console.log('AutocompleteElement', JSON.parse(complexItemsParsed));
-    //     // const html2 = html` <vl-autocomplete
-    //     //     data-vl-min-chars="1"
-    //     //     items=${complexItemsParsed}
-    //     //     placeholder="Hint: typ Gent"
-    //     // >
-    //     // </vl-autocomplete>`;
     //     this.innerHTML = `
     //         <div class="wrapper">
     //             <div class="container">
@@ -46,36 +35,6 @@ export class AutocompleteElement extends LitElement {
     //         </div>
     //   `;
     // }
-
-    render() {
-        const title = 'Autocomplete';
-        const complexItems = [
-            { title: 'Gent', subtitle: 'Gemeente', value: '1' },
-            { title: 'Gentbos, Merelbeke', subtitle: 'Adres', value: '2' },
-            { title: 'Gentbruggestraat, Gent', subtitle: 'Adres', value: '3' },
-            { title: 'Gentele, Brugge', subtitle: 'Adres', value: '5' },
-            { title: 'Automotive Contractors Gent ', subtitle: 'Project', value: '6' },
-            { title: 'Buurtshuis Watersportbaan Gent', subtitle: 'Project', value: '7' },
-        ];
-        // const complexItemsParsed = JSON.stringify(complexItems);
-        return html`
-            <div class="wrapper">
-                <div class="container">
-                    <h2 is="vl-h2" data-vl-alt data-vl-no-space-bottom>${title}</h2>
-                    <br />
-                    <vl-autocomplete
-                        data-vl-min-chars="1"
-                        data-vl-max-suggestions="5"
-                        data-vl-caption-format="title-subtitle-vertical"
-                        data-vl-group-by="title"
-                        .items=${complexItems}
-                        placeholder="Hint: typ Gent"
-                    >
-                    </vl-autocomplete>
-                </div>
-            </div>
-        `;
-    }
 }
 
 customElements.define('exhibit-autocomplete', AutocompleteElement);
