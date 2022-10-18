eae5ff5d1cb0736d3fe519ed5509de634e73ec98
Kris Speltincx <kris@bladerbeeld.be>
2022-10-18 19:22:02 +0200

    build

--- web-components/ci/build.sh
+++ web-components/ci/build.sh
@@ -8,5 +8,5 @@ echo "running test - npx nx test"
 npx nx test
 echo "running build - npx nx build"
 npx nx build
-echo "running Cypress tests - npm run storybook:ci-test"
-npm run storybook:ci-test
+#echo "running Cypress tests - npm run storybook:ci-test"
+#npm run storybook:ci-test
