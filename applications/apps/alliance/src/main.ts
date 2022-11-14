bf25b33b4b18e4b2806e232ff64d628ef40e54db
Kris Speltincx <kris@bladerbeeld.be>
2022-11-14 12:14:04 +0100

    feat: post process sections
    - .css omzetten naar .scss.js
    - .lib.js toevoegen aan dist van elements en components
    - bij het omzetten .css naar .scss.js moet 1 \ er 2 worden (om de escaping te escapen)

--- applications/apps/alliance/src/main.ts
+++ applications/apps/alliance/src/main.ts
@@ -1,1 +1,8 @@
+// @uig imports
+import '@domg-lib/elements';
+import '@domg-lib/components';
+
+// app components
 import './app/app.element.ts';
+import './app/component/breadcrumb.element.ts';
+import './app/component/toaster.element.ts';
---
+++ web-components/post-process-libs.mjs
@@ -0,0 +1,56 @@
+import path from 'path';
+import fs from 'fs-extra';
+
+const wrapAsStylesObject = (content) => `const styles = \`${content}\`; export default styles;`;
+
+const wrapCssInJs = (directoryToSearch) => {
+    const pattern = '.css';
+    fs.readdirSync(directoryToSearch).forEach((subDirectory) => {
+        const filePath = path.resolve(directoryToSearch, subDirectory);
+        const stat = fs.statSync(filePath);
+        if (stat.isDirectory()) {
+            wrapCssInJs(filePath);
+        }
+        if (stat.isFile() && filePath.endsWith(pattern)) {
+            const scssJsFile = filePath.replace(/.css$/, '.scss.js');
+            fs.readFile(filePath, 'utf8',(err, data) => {
+                // replace one \ with two \\ to escape the escape character
+                data = data.replace(/\\/g, '\\\\');
+                fs.outputFile(scssJsFile, wrapAsStylesObject(data));
+                console.log('processed css file to:', scssJsFile);
+                // remove the original files, keep the map file
+                fs.unlink(filePath);
+            });
+        }
+    });
+};
+
+const copyLibJs = (directoryToSearch, directoryCopyTo) => {
+    const pattern = '.lib.js';
+    fs.readdirSync(directoryToSearch).forEach((subDirectory) => {
+        const filePath = directoryToSearch + '/' + subDirectory;
+        const copyPath = directoryCopyTo + '/' + subDirectory;
+        const stat = fs.statSync(filePath);
+        if (stat.isDirectory()) {
+            copyLibJs(filePath, copyPath);
+        }
+        if (stat.isFile() && filePath.endsWith(pattern)) {
+            console.log('filePath:', filePath);
+            fs.copy(filePath, copyPath, err => {
+                console.log('copied lib to:', copyPath);
+            })
+        }
+    });
+};
+
+// post process elements
+copyLibJs('libs/elements/src/lib', 'dist/libs/elements/src/lib');
+
+// post process components
+wrapCssInJs('dist/libs/components/src/lib');
+copyLibJs('libs/components/src/lib', 'dist/libs/components/src/lib');
+
+// post process sections
+wrapCssInJs('dist/libs/sections/src/lib');
+copyLibJs('libs/sections/src/lib', 'dist/libs/sections/src/lib');
+
