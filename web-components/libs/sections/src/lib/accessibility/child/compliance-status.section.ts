ac489363c3f6bf263ee217f99ea9f720a628bc6e
Karim Dehbi <karim.dehbi@gmail.com>
2023-01-04 10:51:39 +0100

    fix: UIG-2251: ensure Accessibility component is again working as in production, evaluation & compliance status were not properly passed through generated child templates

--- web-components/libs/sections/src/lib/accessibility/child/compliance-status.section.ts
+++ web-components/libs/sections/src/lib/accessibility/child/compliance-status.section.ts
@@ -7,18 +7,14 @@ export const complianceStatus = ({ complianceStatus, evaluationStatus }: Accessi
         switch (complianceStatus) {
             case COMPLIANCE_STATUS.FULLY_COMPLIANT:
                 return html`Deze website voldoet volledig aan de ${wcagLink()}.`;
-                break;
             case COMPLIANCE_STATUS.PARTIALLY_COMPLIANT:
                 return html`Deze website voldoet gedeeltelijk aan de ${wcagLink()} omdat nog niet aan de onderstaande
                 eisen is voldaan.`;
-                break;
             case COMPLIANCE_STATUS.NOT_COMPLIANT:
                 return html`Deze website voldoet niet aan de ${wcagLink()} omdat nog niet aan de onderstaande eisen is
                 voldaan.`;
-                break;
             default:
                 return null;
-                break;
         }
     };
     return html` <div id="compliance-status" is="vl-column" data-vl-size="12" data-vl-medium-size="12">
